6a83cb93fb2e8cdb88947636f54eb5fb3f738b73
Deep Bag <deep.bag@emergys.com>
2025-04-04 12:04:13 +0530

    added on success

--- src/use-query/query-client.tsx
+++ src/use-query/query-client.tsx
@@ -1,14 +1,23 @@
 import { MutationCache, QueryCache, QueryClient } from "@tanstack/react-query";
-import { setErrorMessageRedux } from "redux/Message/message.slice";
+import {
+  setErrorMessageRedux,
+  setSuccessMessageRedux,
+} from "redux/Message/message.slice";
 import store from "redux/store";
 
 export const queryClient = new QueryClient({
   queryCache: new QueryCache({
+    // onSuccess: (data: any) => {
+    //   store.dispatch(setSuccessMessageRedux("Query success 🎉"));
+    // },
     onError: (error: { message: string }) => {
       store.dispatch(setErrorMessageRedux(error.message));
     },
   }),
   mutationCache: new MutationCache({
+    onSuccess: (data: any) => {
+      store.dispatch(setSuccessMessageRedux("Mutation success 🎉"));
+    },
     onError: (error: { message: string }) => {
       store.dispatch(setErrorMessageRedux(error.message));
     },
